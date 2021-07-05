be522f788a8188cd615db4b0d2b8b4ae140d2ef9
Maculos <63527367+Maculos@users.noreply.github.com>
2021-07-05 12:49:17 -0500

    finally a working embed.

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from discord.ext import commands
 
 load_dotenv()
 token = environ["TOKEN"]
-#bot = discord.Client()
-bot = commands.Bot(command_prefix = '!')
+#bot = discord.Client() 
+bot = commands.Bot(command_prefix = '/') #idk how to integrate it with the / commands, to google
+
+@bot.event
+async def on_ready():
+    print('Connected as {0.user}'.format(bot))
 
 @bot.command()
-async def embed(ctx):
-    embed=discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/", description="This is an embed that will show how to build an embed and the different components", color=discord.Color.blue())
+async def source(ctx):
+    embed=discord.Embed(title="View Source", url="https://github.com/maculos/dojacat/", description="View this project on Github.", color=discord.Color.magenta())
     await ctx.send(embed=embed)
 
 @bot.event
@@ -18,8 +22,8 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if message.content.startswith('embed'):
-        await embed(message.channel)
+        await source(message.channel)
     if message.content.startswith('hello'):
         await message.channel.send('Hallo!')
 
-bot.run(token, bot=True, reconnect=False)
+bot.run(token)
